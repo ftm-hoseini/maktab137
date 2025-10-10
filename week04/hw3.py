43c4de20f29d93da03a984b58244f46b8c5e94cf
Fatemeh_Sadat <fatemehsadat1380@gmail.com>
2025-10-10 12:57:16 +0330

    add hw3: most repetitions

--- week04/hw3.py
+++ week04/hw3.py
@@ -7,15 +7,15 @@ def most_repetitions(my_list: list):
         else:
             new_dict[item] = 1
 
-    res = max(new_dict.values())
+    max_number = max(new_dict.values())
 
     for k, v in new_dict.items():
-        if v == res:
+        if v == max_number:
             print(f"{k} has the most repetitions.")
 
   
         
-the_list = [1, 2, 3, 3, 4, 5, 2, 5, 6, 5, 2, 2]
+the_list = [1, 2, 3, 3, 4, 5, 2, 5, 6, 5,"a","b","a","a","a"]
 most_repetitions(the_list)
 
         
